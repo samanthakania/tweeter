6f3764295808632f089e3b7b2ee4647e8a72ae61
Samantha Kania <samantha.kania12@gmail.com>
2019-02-21 19:56:31 +0000

    mongo

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,27 +1,24 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+//const simulateDelay = require("./util/simulate-delay");
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+    saveTweet: function (newTweet, callback) {
+      db.collection('tweets').insertOne(newTweet);
         callback(null, true);
-      });
     },
-
-    // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
-    }
-
-  };
+      const sortNewestFirst = (a,b) => a.created_at - b.created_at;
+    db.collection("tweets").find().toArray((err, tweets) => {
+          callback(null, tweets.sort(sortNewestFirst));
+    });
+   }
+ };
 }
